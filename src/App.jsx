8e04762a791d2b437e02800782936d318a808d43
Miqueias Sousa <miqueiassousa80@gmail.com>
2022-12-12 16:41:23 -0300

    Componente: Sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,27 @@
 import { Header } from './components/Header'
 import { Post } from './components/Post'
+import { Sidebar } from './components/Sidebar'
 
+import styles from './App.module.css'
 import './styles.css'
 
 function App() {
   return (
     <div>
       <Header />
-      <Post
-        author="Miqueias Sousa"
-        content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus laborum et recusandae. Molestias quisquam aliquam culpa vitae fugit sed facilis, itaque minima ipsum deserunt. Veniam facilis autem modi numquam dolorem!"
-      />
-      <Post
-        author="Diego Fernandes"
-        content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos repellendus velit, quia accusamus placeat eaque aliquid id esse animi sint et ducimus possimus unde pariatur cumque totam porro exercitationem nobis!"
-      />
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          <Post
+            author="Miqueias Sousa"
+            content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus laborum et recusandae. Molestias quisquam aliquam culpa vitae fugit sed facilis, itaque minima ipsum deserunt. Veniam facilis autem modi numquam dolorem!"
+          />
+          <Post
+            author="Diego Fernandes"
+            content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos repellendus velit, quia accusamus placeat eaque aliquid id esse animi sint et ducimus possimus unde pariatur cumque totam porro exercitationem nobis!"
+          />
+        </main>
+      </div>
     </div>
   )
 }
