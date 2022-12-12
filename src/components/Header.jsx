f4f23565a6523e7862be412d5dff1aee51200cc0
Miqueias Sousa <miqueiassousa80@gmail.com>
2022-12-12 16:16:07 -0300

    Componente: Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,11 @@
 import styles from './Header.module.css'
 
+import igniteFeedLogo from '../assets/ignite-feed-logo.svg'
+
 export function Header() {
   return (
     <header className={styles.header}>
-      <strong>Ignite Feed</strong>
+      <img src={igniteFeedLogo} alt="Ignite Feed logotipo" />
     </header>
   )
 }
