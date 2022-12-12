b936f7ecc3be57cf0df0c59f9b0ad4ee78a30362
Miqueias Sousa <miqueiassousa80@gmail.com>
2022-12-12 16:59:17 -0300

    Finalizando Sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,3 +1,4 @@
+import { PencilLine } from 'phosphor-react'
 import styles from './Sidebar.module.css'
 
 export function Sidebar() {
@@ -8,11 +9,18 @@ export function Sidebar() {
         src="https://images.unsplash.com/photo-1457305237443-44c3d5a30b89?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=80"
       />
       <div className={styles.profile}>
+        <img
+          className={styles.avatar}
+          src="https://github.com/miqueiasousa.png"
+        />
         <strong>Miqueias Sousa</strong>
         <span>Web Developer</span>
       </div>
       <footer>
-        <a href="#">Editar seu perfil</a>
+        <a href="#">
+          <PencilLine size={24} />
+          <span>Editar seu perfil</span>
+        </a>
       </footer>
     </aside>
   )
