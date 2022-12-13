ec72e534a20ba17c8e6b30f16a4df39da80ea14f
Miqueias Sousa <miqueiassousa80@gmail.com>
2022-12-13 19:03:29 -0300

    Componente: Post

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,8 +1,38 @@
+import styles from './Post.module.css'
+
 export function Post({ author, content }) {
   return (
-    <div>
-      <strong>{author}</strong>
-      <p>{content}</p>
-    </div>
+    <article className={styles.post}>
+      <header>
+        <div className={styles.author}>
+          <img
+            className={styles.avatar}
+            src="https://images.unsplash.com/photo-1457305237443-44c3d5a30b89?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=80"
+          />
+          <div className={styles.authorInfo}>
+            <strong>Miqueias Sousa</strong>
+            <span>Web Developer</span>
+          </div>
+        </div>
+        <time title="11 de Maio às 08:13h" dateTime="2022-05-11 08:13:30">
+          Publicado há 1h
+        </time>
+      </header>
+      <div className={styles.content}>
+        <p>Fala galeraa 👋</p>
+        <p>
+          Acabei de subir mais um projeto no meu portifa. É um projeto que fiz
+          no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀
+        </p>
+        <p>
+          👉 <a href="jane.design/doctorcare">jane.design/doctorcare</a>
+        </p>
+
+        <p>
+          <a href="#">#novoprojeto</a> <a href="#">#nlw</a>{' '}
+          <a href="#">#rocketseat</a>
+        </p>
+      </div>
+    </article>
   )
 }
